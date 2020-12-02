ce9289c0b830d4d6e4927b1f401d825e8e7a92f6
Horki <ivanzvonimirhorvat@gmail.com>
2020-12-02 14:30:43 +0100

    airplanes 6: cc, update to class

--- cpp/cc_lab06.cc
+++ cpp/cc_lab06.cc
@@ -4,52 +4,50 @@
 #include <thread>
 #include <vector>
 
-static constexpr size_t NO_TH = 5;
-static sem_t sem_bin;
+class Semaphore {
+  sem_t sem_bin;
+  std::vector<std::thread> threads;
+  std::size_t no;
 
-void init_semaphore();
-void close_semaphore();
-void airplanes(const size_t);
-void wait_threads(std::vector<std::thread> &);
-
-int main() {
-  init_semaphore();
-  std::vector<std::thread> threads(NO_TH);
-  for (size_t i = 0; i < NO_TH; ++i) {
-    threads.push_back(std::thread(airplanes, i + 1));
+ public:
+  Semaphore(std::size_t no) : threads(no), no(no) {
+    // Init semaphore
+    sem_init(&sem_bin, 0, 1);
+    add_airplanes();
   }
-  wait_threads(threads);
-  close_semaphore();
-}
 
-void airplanes(const size_t n) {
-  std::cout << "Airplane " << n << " => Control tower: permission to land\n";
-  sem_wait(&sem_bin);
-  std::cout << "Control tower => Airplane " << n << ": Permission granted\n";
-  std::cout << "Airplane " << n
-            << " => Control tower: I have landed the runway\n";
-  std::cout << "Control tower => everyone: Runway is available\n";
-  sem_post(&sem_bin);
-}
+  ~Semaphore() {
+    sem_destroy(&sem_bin);
+    std::cout << "semaphore done" << std::endl;
+  }
 
-void wait_threads(std::vector<std::thread> &threads) {
-  for (auto &t : threads) {
-    if (t.joinable()) {
-      t.join();
+  void wait() {
+    for (auto &t : threads) {
+      if (t.joinable()) {
+        t.join();
+      }
     }
   }
-}
 
-void init_semaphore() {
-  // init (binary|lock semaphore) with shared between threads of process
-  // 1 is for unlocked state
-  if (sem_init(&sem_bin, 0, 1) != 0) {
-    std::cerr << "Error init binary semaphore" << std::endl;
+ private:
+  void add_airplanes() {
+    for (std::size_t i = 0; i < no; ++i) {
+      threads.push_back(std::thread([=]() {
+        std::cout << "Airplane " << i
+                  << " => Control tower: permission to land\n";
+        sem_wait(&sem_bin);
+        std::cout << "Control tower => Airplane " << i
+                  << ": Permission granted\n";
+        std::cout << "Airplane " << i
+                  << " => Control tower: I have landed the runway\n";
+        std::cout << "Control tower => everyone: Runway is available\n";
+        sem_post(&sem_bin);
+      }));
+    }
   }
-}
+};
 
-void close_semaphore() {
-  if (sem_destroy(&sem_bin) != 0) {
-    std::cerr << "Error destroying binary semaphore" << std::endl;
-  }
+int main() {
+  Semaphore semaphore(5);
+  semaphore.wait();
 }
