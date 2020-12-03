3b1eb98c41bd70598cfe1e47a502279089f4db25
Horki <ivanzvonimirhorvat@gmail.com>
2020-12-03 10:34:55 +0100

    lab02a: cc; small fixes

--- cpp/cc_lab02_a.cc
+++ cpp/cc_lab02_a.cc
@@ -3,11 +3,12 @@
 
 class Data {
  private:
-  size_t a, b, c, total;
+  std::size_t a, b, c, total;
 
  public:
   Data() : a(10), b(10), c(10), total(0) {}
-  Data(size_t a, size_t b, size_t c) : a(a), b(b), c(c), total(0) {}
+  Data(std::size_t a, std::size_t b, std::size_t c)
+      : a(a), b(b), c(c), total(0) {}
   // Expensive operation O(n^3)
   void calculate() {
     for (size_t i = 1; i <= a; ++i) {
@@ -18,14 +19,15 @@ class Data {
       }
     }
   }
-  size_t get_total() const { return total; }
+  std::size_t get_total() const { return total; }
 };
 
 int main() {
   Data d;
+  std::cout << "Calculate in separate thread" << std::endl;
   std::thread t([&] { d.calculate(); });
-  std::cout << "Waiting thread to finish\n";
+  std::cout << "Waiting thread to finish" << std::endl;
   t.join();
-  std::cout << "Thread finished\n";
+  std::cout << "Thread finished" << std::endl;
   std::cout << "Result = " << d.get_total() << std::endl;
 }
