88316c83258ceb849edba7b1db1406c20e9dc0cf
Horki <ivanzvonimirhorvat@gmail.com>
2020-12-05 20:19:09 +0100

    cc: lab02; b, updates

--- cpp/cc_lab02_b.cc
+++ cpp/cc_lab02_b.cc
@@ -1,56 +1,84 @@
 #include <algorithm>  // copy
+#include <array>      // array
 #include <iostream>   // cout
 #include <iterator>   // ostream_iterator
 #include <numeric>    // iota
 #include <thread>     // thread
-#include <vector>     // vecot
+#include <vector>     // vector
 
-static constexpr size_t N = 15;
+namespace Custom {
+using Iter = std::ostream_iterator<int>;
 
-std::vector<std::vector<int>> v1(N, std::vector<int>(N, 2000));
-std::vector<std::vector<int>> v2(N, std::vector<int>(N));
-std::vector<std::vector<int>> res(N, std::vector<int>(N));
+template <int N, int M>
+class Matrix {
+ private:
+  std::array<int, N * M> arr;
+  std::size_t n;
+  std::size_t m;
 
-void init();
-void print_matrix(const std::vector<std::vector<int>> &);
-void calculate(const size_t);
-
-int main() {
-  init();
-  std::vector<std::thread> threads(N);
-  std::cout << "Matrix A:\n";
-  print_matrix(v1);
-  std::cout << "Matrix B:\n";
-  print_matrix(v2);
-  for (size_t i = 0; i < N; ++i) {
-    threads.push_back(std::thread(calculate, i));
-  }
-  for (auto &th : threads) {
-    if (th.joinable()) {
-      th.join();
+ public:
+  Matrix() : n(N), m(M) {
+    for (int i = 0; i < n; ++i) {
+      std::iota(arr.begin() + (m * i), arr.begin() + ((m * i) + m), 1);
     }
   }
-  std::cout << "Matrix A + B\n";
-  print_matrix(res);
-}
 
-void print_matrix(const std::vector<std::vector<int>> &v) {
-  for (const auto &row : v) {
-    std::copy(row.begin(), row.end(),
-              std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
+  Matrix(int val) : n(N), m(M) { std::fill(arr.begin(), arr.end(), val); }
+
+  constexpr int const &operator()(const std::size_t x,
+                                  const std::size_t y) const {
+    return arr.at(x * m + y);
   }
-}
 
-void init() {
-  for (auto &row : v2) {
-    std::iota(row.begin(), row.end(), 1);
+  constexpr int &operator()(const std::size_t x, const std::size_t y) {
+    return arr.at(x * m + y);
   }
-}
 
-void calculate(const size_t i) {
-  std::cout << "Calculating in thread id:" << i << std::endl;
-  for (size_t j = 0; j < N; ++j) {
-    res[i][j] = v1[i][j] + v2[i][j];
+  constexpr std::size_t rows() const { return n; }
+
+  constexpr std::size_t cols() const { return m; }
+
+  friend std::ostream &operator<<(std::ostream &os, const Matrix<N, M> &m) {
+    for (int i = 0; i < m.rows(); ++i) {
+      std::copy(m.arr.begin() + (m.cols() * i),
+                m.arr.begin() + ((m.cols() * i) + m.cols()), Iter(os, " "));
+      os << std::endl;
+    }
+    return os;
+  }
+};
+}  // namespace Custom
+
+int main() {
+  try {
+    constexpr std::size_t rows{15};
+    constexpr std::size_t cols{15};
+    std::vector<std::thread> threads;
+    threads.reserve(rows);
+    Custom::Matrix<rows, cols> a;
+    Custom::Matrix<rows, cols> b(2000);
+    Custom::Matrix<rows, cols> res(0);
+    std::cout << "A:" << std::endl;
+    std::cout << a;
+    std::cout << "B: " << std::endl;
+    std::cout << b;
+    for (std::size_t row = 0; row < rows; ++row) {
+      threads.push_back(std::thread([&, row] {
+        for (std::size_t col = 0; col < cols; ++col) {
+          res(row, col) += a(row, col) + b(row, col);
+        }
+      }));
+    }
+    std::cout << "wait for join threads" << std::endl;
+    for (auto &thread : threads) {
+      thread.join();
+    }
+    std::cout << "result: " << std::endl;
+    std::cout << res;
+
+  } catch (const std::out_of_range &e) {
+    std::cerr << "range: " << e.what() << std::endl;
+  } catch (...) {
+    std::cerr << "something else" << std::endl;
   }
 }
