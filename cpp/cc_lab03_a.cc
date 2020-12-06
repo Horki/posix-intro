67b187a218478e800f73664f8ed3fe9fbfb1a9d3
Horki <ivanzvonimirhorvat@gmail.com>
2020-12-06 14:03:41 +0100

    cc: lab03a; rewrite, c++ style

--- cpp/cc_lab03_a.cc
+++ cpp/cc_lab03_a.cc
@@ -1,41 +1,31 @@
+#include <iomanip>   // setfill, setw
 #include <iostream>  // cout
 #include <mutex>     // mutex, unique_lock
 #include <thread>    // thread
 #include <vector>    // vector
 
-static constexpr size_t NO_TH = 10;
-static constexpr size_t N_MAX = 50;
-static size_t counter = 1;
-static std::mutex mut;
-
-void func1(const size_t);
-void func2(const size_t);
-
 int main() {
-  std::vector<std::thread> threads(NO_TH);
-  for (size_t i = 0; i < NO_TH;) {
-    threads.push_back(std::thread(func1, i++));
-    threads.push_back(std::thread(func2, i++));
+  constexpr std::size_t no_th{10};
+  constexpr std::size_t n_max{50};
+  std::size_t counter{1};
+  std::mutex mut;
+  std::vector<std::thread> threads(no_th);
+  auto func = [&cnt = counter, &m = mut, max = n_max](
+                  std::size_t const fun_id, std::size_t const n) -> void {
+    std::unique_lock<std::mutex> lock{m};
+    for (std::size_t i{0}; i < max; ++i) {
+      std::cout << "Function " << fun_id << " = " << std::setfill('0')
+                << std::setw(3) << cnt++ << "; Thread " << std::setfill('0')
+                << std::setw(2) << (n + 1) << std::endl;
+    }
+  };  // unlock lock
+  for (std::size_t i{0}; i < no_th;) {
+    threads.push_back(std::thread(func, 1, i++));
+    threads.push_back(std::thread(func, 2, i++));
   }
-  for (auto& t : threads) {
+  for (auto &t : threads) {
     if (t.joinable()) {
       t.join();
     }
-  }
-}
-
-void func1(const size_t n) {
-  std::unique_lock<std::mutex> lock{mut};
-  for (size_t i = 0; i < N_MAX; ++i) {
-    std::cout << "Function 1 = " << counter++ << "; Thread " << (n + 1)
-              << std::endl;
-  }
-}
-
-void func2(const size_t n) {
-  std::unique_lock<std::mutex> lock{mut};
-  for (size_t i = 0; i < N_MAX; ++i) {
-    std::cout << "Function 2 = " << counter++ << "; Thread " << (n + 1)
-              << std::endl;
   }
 }
