170c4449990c7f49675e8276e9b9c966e4250d38
Horki <ivanzvonimirhorvat@gmail.com>
2020-12-17 15:06:52 +0100

    c: ipc; two way pipe update

--- c/ipc/message_passing/pipe/twowaypipe.c
+++ c/ipc/message_passing/pipe/twowaypipe.c
@@ -1,17 +1,19 @@
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>  // pipe, read, write, close
 
-#define BUFFER_SIZE 20
+#define STR_BUF 256
 #define PIPES_NO 2
 
 static void create_pipe(int[PIPES_NO], int);
+static void parent_process(int[PIPES_NO], int[PIPES_NO]);
+static void child_process(int[PIPES_NO], int[PIPES_NO]);
 
 int main() {
   int pipe_fds_1[PIPES_NO], pipe_fds_2[PIPES_NO];
-  const char pipe1_write_msg[BUFFER_SIZE] = "Hi from pipe 1";
-  const char pipe2_write_msg[BUFFER_SIZE] = "Hello from pipe 2";
-  char read_msg[BUFFER_SIZE];
 
   create_pipe(pipe_fds_1, 1);
   create_pipe(pipe_fds_2, 2);
@@ -19,30 +21,9 @@ int main() {
   pid_t pid = fork();
 
   if (pid == 0) {
-    // Parent Process
-    puts("Parent Process");
-    // close unwanted pipe 1 read side
-    close(pipe_fds_1[0]);
-    // close unwanted pipe 2 write side
-    close(pipe_fds_2[1]);
-    printf("[Parent] writing to pipe 1: [%s]\n", pipe1_write_msg);
-    write(pipe_fds_1[1], pipe1_write_msg, sizeof pipe1_write_msg);
-    // Reading from pipe 2
-    read(pipe_fds_2[0], read_msg, sizeof read_msg);
-    printf("[Parent] reading from pipe 2: [%s]\n", read_msg);
+    parent_process(pipe_fds_1, pipe_fds_2);
   } else {
-    // Child Process
-    puts("Child Process");
-    // close unwanted pipe 1 write side
-    close(pipe_fds_1[1]);
-    // close unwanted pipe 2 read side
-    close(pipe_fds_2[0]);
-    // Reading from pipe 1
-    read(pipe_fds_1[0], read_msg, sizeof read_msg);
-    printf("[Child] reading from pipe 1: [%s]\n", read_msg);
-    // Write to pipe 1
-    write(pipe_fds_2[1], pipe2_write_msg, sizeof pipe2_write_msg);
-    printf("[Child] writing to pipe 2: [%s]\n", pipe2_write_msg);
+    child_process(pipe_fds_1, pipe_fds_2);
   }
 
   printf("Pipe %d, done.\n", pid);
@@ -55,4 +36,55 @@ static void create_pipe(int pipe_fds[PIPES_NO], int pipe_id) {
     fprintf(stderr, "unable to create pipe %d\n", pipe_id);
     exit(EXIT_FAILURE);
   }
+}
+
+static void parent_process(int pipe_fds_1[PIPES_NO], int pipe_fds_2[PIPES_NO]) {
+  // Parent Process
+  puts("Parent Process");
+  // close unwanted pipe 1 read side
+  close(pipe_fds_1[0]);
+  // close unwanted pipe 2 write side
+  close(pipe_fds_2[1]);
+  char msg[STR_BUF];
+  while (true) {
+    printf("[PARENT] send message: ");
+    fgets(msg, STR_BUF, stdin);
+    strtok(msg, "\n");
+    printf("[PARENT] writing to pipe 1: [%s]\n", msg);
+    write(pipe_fds_1[1], msg, sizeof msg);
+    // Reading from pipe 2
+    read(pipe_fds_2[0], msg, sizeof msg);
+    printf("[PARENT] reading from pipe 2: [%s]\n", msg);
+    if (strncmp(msg, "0", 1) == 0) {
+      write(pipe_fds_1[1], msg, sizeof msg);
+      break;
+    }
+  }
+  puts("PARENT done");
+}
+
+static void child_process(int pipe_fds_1[PIPES_NO], int pipe_fds_2[PIPES_NO]) {
+  // Child Process
+  puts("Child Process");
+  // close unwanted pipe 1 write side
+  close(pipe_fds_1[1]);
+  // close unwanted pipe 2 read side
+  close(pipe_fds_2[0]);
+  // Reading from pipe 1
+  char msg[STR_BUF];
+  while (true) {
+    read(pipe_fds_1[0], msg, sizeof msg);
+    if (strncmp(msg, "0", 1) == 0) {
+      write(pipe_fds_2[1], msg, sizeof msg);
+      break;
+    }
+    printf("[CHILD] reading from pipe 1: [%s]\n", msg);
+    // Write to pipe 1
+    printf("[CHILD] send message: ");
+    fgets(msg, STR_BUF, stdin);
+    strtok(msg, "\n");
+    write(pipe_fds_2[1], msg, sizeof msg);
+    printf("[Child] writing to pipe 2: [%s]\n", msg);
+  }
+  puts("CHILD done");
 }
