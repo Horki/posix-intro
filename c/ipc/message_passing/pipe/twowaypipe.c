cf15a401fa834de78e4667a28505edc06402e9e8
Horki <ivanzvonimirhorvat@gmail.com>
2020-12-18 11:48:06 +0100

    c: ipc: 2 way pipe

--- c/ipc/message_passing/pipe/twowaypipe.c
+++ c/ipc/message_passing/pipe/twowaypipe.c
@@ -1,22 +1,33 @@
 #include <signal.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>  // pipe, read, write, close
+#include <stdbool.h>  // bool
+#include <stdio.h>    // puts, printf, fgets, fprintf
+#include <stdlib.h>   // EXIT_SUCCESS, EXIT_FAILURE, exit
+#include <string.h>   // strtok, strncmp
+#include <unistd.h>   // pipe, read, write, close, fork, pid_t
 
 #define STR_BUF 256
 #define PIPES_NO 2
 
+static bool running = true;
+
+static int catch_signal(int sig, void (*)(int));
+static void close_write(int);
 static void create_pipe(int[PIPES_NO], int);
 static void parent_process(int[PIPES_NO], int[PIPES_NO]);
 static void child_process(int[PIPES_NO], int[PIPES_NO]);
 
 int main() {
+  // SIGINT handler
+  if (catch_signal(SIGINT, close_write) == -1) {
+    fprintf(stderr, "Can't map the handler");
+    exit(EXIT_FAILURE);
+  }
   int pipe_fds_1[PIPES_NO], pipe_fds_2[PIPES_NO];
 
   create_pipe(pipe_fds_1, 1);
   create_pipe(pipe_fds_2, 2);
+
+  puts("Added SIGINT handler!!!");
 
   pid_t pid = fork();
 
@@ -48,9 +59,13 @@ static void parent_process(int pipe_fds_1[PIPES_NO], int pipe_fds_2[PIPES_NO]) {
   char msg[STR_BUF];
   while (true) {
     printf("[PARENT] send message: ");
-    fgets(msg, STR_BUF, stdin);
-    strtok(msg, "\n");
-    printf("[PARENT] writing to pipe 1: [%s]\n", msg);
+    if (running) {
+      fgets(msg, STR_BUF, stdin);
+      strtok(msg, "\n");
+      printf("[PARENT] writing to pipe 1: [%s]\n", msg);
+    } else {
+      strncpy(msg, "0", 1);
+    }
     write(pipe_fds_1[1], msg, sizeof msg);
     // Reading from pipe 2
     read(pipe_fds_2[0], msg, sizeof msg);
@@ -60,6 +75,10 @@ static void parent_process(int pipe_fds_1[PIPES_NO], int pipe_fds_2[PIPES_NO]) {
       break;
     }
   }
+  // close unwanted pipe 1 write side
+  close(pipe_fds_1[1]);
+  // close unwanted pipe 2 read side
+  close(pipe_fds_2[0]);
   puts("PARENT done");
 }
 
@@ -81,10 +100,31 @@ static void child_process(int pipe_fds_1[PIPES_NO], int pipe_fds_2[PIPES_NO]) {
     printf("[CHILD] reading from pipe 1: [%s]\n", msg);
     // Write to pipe 1
     printf("[CHILD] send message: ");
-    fgets(msg, STR_BUF, stdin);
-    strtok(msg, "\n");
+    if (running) {
+      fgets(msg, STR_BUF, stdin);
+      strtok(msg, "\n");
+    } else {
+      strncpy(msg, "0", 1);
+    }
     write(pipe_fds_2[1], msg, sizeof msg);
     printf("[Child] writing to pipe 2: [%s]\n", msg);
   }
+  // close unwanted pipe 1 read side
+  close(pipe_fds_1[0]);
+  // close unwanted pipe 2 write side
+  close(pipe_fds_2[1]);
   puts("CHILD done");
+}
+
+static int catch_signal(int sig, void (*handler)(int)) {
+  struct sigaction action;
+  action.sa_handler = handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  return sigaction(sig, &action, NULL);
+}
+
+static void close_write(int sig) {
+  fprintf(stderr, "Signal: %d, closing pipeline process\n", sig);
+  running = false;
 }
