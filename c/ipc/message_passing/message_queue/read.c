6290f150e76f5e684a5b41ea2e88513e01bb7f97
Horki <ivanzvonimirhorvat@gmail.com>
2020-12-20 12:04:54 +0100

    c: ipc; m queue fix

--- c/ipc/message_passing/message_queue/read.c
+++ c/ipc/message_passing/message_queue/read.c
@@ -1,23 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <signal.h>   // catch_signal
+#include <stdbool.h>  // true
+#include <stdio.h>    // puts, fprintf, printf
+#include <stdlib.h>   // EXIT_SUCCESS, EXIT_FAILURE, exit
+#include <string.h>   // strncmp
 #include <sys/ipc.h>  // IPC_RMID
 #include <sys/msg.h>  // msgget, msgrcv, msgctl
 
-#include "type.h"  // msg, create_queue
+#include "type.h"  // msg, create_queue, close_ipc
 
 int main() {
   puts("Read from IPC message queue");
   puts("Run ./bin/c/c_lab07_ipc_mq_write to send to IPC MQ");
 
+  // SIGINT handler
+  if (catch_signal(SIGINT, close_ipc) == -1) {
+    fprintf(stderr, "Can't map the handler");
+    exit(EXIT_FAILURE);
+  }
+
   int message_id = create_queue();
 
   // receive the message from queue
-  msgrcv(message_id, &msg, sizeof msg, 1, 0);
+  while (true) {
+    msgrcv(message_id, &msg, sizeof msg, 1, 0);
 
-  // Display the message
-  printf("Data received from queue: %s\n", msg.text);
+    // Display the message
+    printf("Data received from queue: %s\n", msg.text);
+    if (strncmp(msg.text, "0", 1) == 0) {
+      break;
+    }
+  }
 
-  // Destroy the message queue
   puts("Destroy the message queue!!!");
   msgctl(message_id, IPC_RMID, NULL);
 
--- c/ipc/message_passing/message_queue/write.c
+++ c/ipc/message_passing/message_queue/write.c
@@ -1,24 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/msg.h>  // msgget, msgsend
+#include <signal.h>   // catch_signal
+#include <stdbool.h>  // true
+#include <stdio.h>    // puts, fprintf, fgets
+#include <stdlib.h>   // EXIT_SUCCESS, EXIT_FAILURE, exit
+#include <string.h>   // strtok, strncmp
+#include <sys/msg.h>  // msgget, msgsnd
 
-#include "type.h"  // msg, BUFFER_SIZE, create_queue
+#include "type.h"  // msg, BUFFER_SIZE, create_queue, catch_signal, close_ipc
 
 int main() {
   puts("Write to IPC message queue");
   puts("Run ./bin/c/c_lab07_ipc_mq_read to read from IPC MQ");
 
+  // SIGINT handler
+  if (catch_signal(SIGINT, close_ipc) == -1) {
+    fprintf(stderr, "Can't map the handler");
+    exit(EXIT_FAILURE);
+  }
+
   int message_id = create_queue();
   msg.type = 1;
 
-  puts("Insert data: ");
-  fgets(msg.text, BUFFER_SIZE, stdin);
-
-  // send message to message queue
-  msgsnd(message_id, &msg, sizeof msg, 0);
-
-  // Display the message
-  printf("Data sent to queue %s\n", msg.text);
+  while (true) {
+    printf("Insert data: ");
+    fgets(msg.text, BUFFER_SIZE, stdin);
+    strtok(msg.text, "\n");
+    // send message to message queue
+    msgsnd(message_id, &msg, sizeof msg, 0);
+    // Display the message
+    printf("Data sent to queue '%s'\n", msg.text);
+    if (strncmp(msg.text, "0", 1) == 0) {
+      break;
+    }
+  }
 
   return EXIT_SUCCESS;
 }
