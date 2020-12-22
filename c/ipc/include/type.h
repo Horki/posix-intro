5d0fc631def3e9ae2c7741de166ab12f0a72c22a
Horki <ivanzvonimirhorvat@gmail.com>
2020-12-22 09:26:41 +0100

    c: ipc; mqueue fix

--- c/ipc/include/type.h
+++ c/ipc/include/type.h
@@ -1,7 +1,12 @@
 #ifndef TYPE_H_
 #define TYPE_H_
 
-#include <sys/ipc.h>
+#include <signal.h>   // struct sigaction, sigemptyset, sigaction
+#include <stdbool.h>  // true
+#include <stdio.h>    // fprintf
+#include <string.h>   // strncpy
+#include <sys/ipc.h>  // key_y, ftok, IPC_CREAT
+#include <sys/msg.h>  // msgget, msgsnd
 
 #define BUFFER_SIZE 20
 #define PROJECT_ID 65
@@ -11,11 +16,29 @@ struct message_buffer {
   char text[BUFFER_SIZE];
 } msg;
 
+static bool running = true;
+
 int create_queue() {
   // ftok to generate a random key
   key_t key = ftok("progfile", PROJECT_ID);
   // msgget creates a message queue, and returns identifier
   return msgget(key, 0666 | IPC_CREAT);
+}
+
+int catch_signal(int sig, void (*handler)(int)) {
+  struct sigaction action;
+  action.sa_handler = handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  return sigaction(sig, &action, NULL);
+}
+
+void close_ipc(int sig) {
+  fprintf(stderr, "\nSignal: %d, closing IPC message passing processes\n", sig);
+  running = false;
+  int msg_id = create_queue();
+  strncpy(msg.text, "0", 1);
+  msgsnd(msg_id, &msg, sizeof msg, 0);
 }
 
 #endif
