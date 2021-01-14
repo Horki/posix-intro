b90b299174e36aa8a3b2482e5e5da915e175797d
Horki <ivanzvonimirhorvat@gmail.com>
2021-01-14 11:18:24 +0100

    cpp: 01; add lambdas

--- cpp/cc_lab01.cc
+++ cpp/cc_lab01.cc
@@ -5,22 +5,45 @@
 #include <sys/wait.h>  // waitpid
 #include <unistd.h>    // getpid
 
+#include <functional>
+
 #include "custom_exception.hh"
 
-void execute_child(pid_t &) noexcept(false);
-
-void wait_child(pid_t &) noexcept(false);
-
-pid_t create_fork() noexcept(false);
-
 int main() {
+  std::function<pid_t(void)> create_fork = []() {
+    pid_t pid{fork()};
+    if (pid == -1) {
+      throw new PIDException("Can't fork process");
+    }
+    return pid;
+  };
   try {
     pid_t pid{create_fork()};
     if (!pid) {
       // child process
+      auto execute_child = [](pid_t &ppid) -> void {
+        if (ppid) {
+          throw PIDException("Not a child process!");
+        }
+        std::cout << "Waiting to close child process (gedit[pid=" << getpid()
+                  << "])" << std::endl;
+        if (system("/usr/bin/gedit") != 0) {
+          throw new OpenException("Can't open gedit");
+        }
+      };
       execute_child(pid);
     } else {
       // parent process
+      auto wait_child = [](pid_t &pid) -> void {
+        if (!pid) {
+          throw new PIDException("Not a parent process!");
+        }
+        int pid_status;
+        if (waitpid(pid, &pid_status, 0) == -1) {
+          throw new PIDException("Error waiting child process");
+        }
+        std::cout << "Child exited with exit code:" << pid_status << std::endl;
+      };
       std::cout << "I'm a parent process[ppid=" << getpid()
                 << " waiting for a child\n";
       wait_child(pid);
@@ -30,34 +53,4 @@ int main() {
   } catch (...) {
     std::cerr << "other error" << std::endl;
   }
-}
-
-void execute_child(pid_t &ppid) noexcept(false) {
-  if (ppid) {
-    throw PIDException("Not a child process!");
-  }
-  std::cout << "Waiting to close child process (gedit[pid=" << getpid() << "])"
-            << std::endl;
-  if (system("/usr/bin/gedit") != 0) {
-    throw new OpenException("Can't open gedit");
-  }
-}
-
-void wait_child(pid_t &pid) noexcept(false) {
-  if (!pid) {
-    throw new PIDException("Not a parent process!");
-  }
-  int pid_status;
-  if (waitpid(pid, &pid_status, 0) == -1) {
-    throw new PIDException("Error waiting child process");
-  }
-  std::cout << "Child exited with exit code:" << pid_status << std::endl;
-}
-
-pid_t create_fork() noexcept(false) {
-  pid_t pid{fork()};
-  if (pid == -1) {
-    throw new PIDException("Can't fork process");
-  }
-  return pid;
 }
